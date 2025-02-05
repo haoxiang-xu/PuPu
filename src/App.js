4e51adf3aa8824dec1a6f453393a824f089343dc
Haoxiang Xu <59581718+haoxiang-xu@users.noreply.github.com>
2025-02-04 16:35:01 -0800

    substruct subproject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return;
-}
+import Control_Panel from "./COMPONENTs/control_panel/control_panel";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Control_Panel />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/COMPONENTs/control_panel/control_panel.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import ollama from "./ollama.png";
+import Chat_Section from "../chat_section/chat_section";
+import Side_Menu from "../side_menu/side_menu";
+
+const R = 30;
+const G = 30;
+const B = 30;
+
+const Control_Panel = ({}) => {
+  const [messages, setMessages] = useState([]);
+  const [sectionStarted, setSectionStarted] = useState(false);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setSectionStarted(true);
+    }
+  }, [messages]);
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "0",
+        left: "0",
+
+        width: "100%",
+        height: "100%",
+
+        backgroundColor: `rgb(${R}, ${G}, ${B})`,
+      }}
+    >
+      <img
+        src={ollama}
+        alt="ollama"
+        style={{
+          transition: "all 0.4s cubic-bezier(0.72, -0.16, 0.2, 1.16)",
+          transform: "translate(-50%, -50%)",
+          position: "fixed",
+
+          bottom: sectionStarted ? "-3px" : "12px",
+          left: "50%",
+
+          width: 72,
+
+          padding: 8,
+          borderRadius: 8,
+          opacity: sectionStarted ? 0 : 0.32,
+        }}
+      />
+      <span
+        style={{
+          transition: "all 0.5s cubic-bezier(0.72, -0.16, 0.2, 1.16)",
+          position: "absolute",
+          transform: "translate(-50%, -50%)",
+
+          top: "calc(50% - 2px)",
+          left: "50%",
+          fontSize: 32,
+          color: sectionStarted ? `rgba(255, 255, 255, 0)` : `rgba(255, 255, 255, 0.32)`,
+        }}
+      >
+        power by Ollama
+      </span>
+      <div
+        className="chat-section-wrapper"
+        style={{
+          position: "absolute",
+          transform: "translate(-50%, -50%)",
+          top: "calc(50% + 2px)",
+          left: "50%",
+
+          height: "100%",
+          width: "45%",
+          minWidth: 512,
+        }}
+      >
+        <Chat_Section messages={messages} setMessages={setMessages} />
+      </div>
+      <Side_Menu />
+    </div>
+  );
+};
+
+export default Control_Panel;
