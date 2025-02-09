a048f61e553280a10453936beb0ee15897e2dda3
Haoxiang Xu <59581718+haoxiang-xu@users.noreply.github.com>
2025-02-08 21:14:19 -0800

    color theme updated

---
+++ src/DATA_MANAGERs/root_config_manager/default_themes.js
@@ -0,0 +1,7 @@
+const dark_theme = {
+  R: 30,
+  G: 27,
+  B: 30,
+};
+
+export { dark_theme };
--- src/DATA_MANAGERs/root_config_manager/root_config_manager.js
+++ src/DATA_MANAGERs/root_config_manager/root_config_manager.js
@@ -3,15 +3,16 @@ import React, { useEffect, useState, useCallback } from "react";
 import { RootConfigContexts } from "./root_config_contexts";
 
 import { chat_room_title_generation_prompt } from "./default_instructions";
+import { dark_theme } from "./default_themes";
 
 const RootConfigManager = ({ children }) => {
   const [instructions, setInstructions] = useState({
     chat_room_title_generation_prompt: chat_room_title_generation_prompt,
   });
   const [RGB, setRGB] = useState({
-    R: 30,
-    G: 26,
-    B: 30,
+    R: dark_theme.R,
+    G: dark_theme.G,
+    B: dark_theme.B,
   });
 
   return (
