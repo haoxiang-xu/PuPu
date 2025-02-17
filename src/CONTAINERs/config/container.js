068cb339e6f7da129be1f12cb418508ac688b9be
Haoxiang Xu <haoxiang-xu>
2025-02-17 13:55:33 -0800

    light theme in progress

--- src/CONTAINERs/config/container.js
+++ src/CONTAINERs/config/container.js
@@ -178,15 +178,21 @@ const ConfigContainer = ({ children }) => {
             border: `1px solid rgba(225, 225, 225, 0)`,
             border_onHover: `1px solid rgba(225, 225, 225, 0.16)`,
             border_onActive: `1px solid rgba(225, 225, 225, 0.32)`,
-            backgroundColor: `rgba(${RGB.R + 30}, ${RGB.G + 30}, ${ RGB.B + 30}, 0)`,
-            backgroundColor_onHover: `rgba(${RGB.R + 32}, ${RGB.G + 32}, ${ RGB.B + 32}, 0.64)`,
-            backgroundColor_onActive: `rgba(${RGB.R + 32}, ${RGB.G + 32}, ${ RGB.B + 32}, 1)`,
+            backgroundColor: `rgba(${RGB.R + 30}, ${RGB.G + 30}, ${
+              RGB.B + 30
+            }, 0)`,
+            backgroundColor_onHover: `rgba(${RGB.R + 32}, ${RGB.G + 32}, ${
+              RGB.B + 32
+            }, 0.64)`,
+            backgroundColor_onActive: `rgba(${RGB.R + 32}, ${RGB.G + 32}, ${
+              RGB.B + 32
+            }, 1)`,
           },
         };
       } else {
         return {
           model_menu: {
-            backgroundColor_onHover: `rgba(225, 225, 225, 0.9)`,
+            backgroundColor_onHover: `rgba(225, 225, 225, 1)`,
             backgroundColor_onActive: `rgba(${RGB.R - 64}, ${RGB.G - 64}, ${
               RGB.B - 64
             }, 0.32)`,
@@ -226,10 +232,12 @@ const ConfigContainer = ({ children }) => {
           input_section: {
             border: `1px solid rgba(225, 225, 225, 0)`,
             border_onHover: `1px solid rgba(0, 0, 0, 0.16)`,
-            border_onActive: `1px solid rgba(0, 0, 0, 0.4)`,
-            backgroundColor: `rgba(${RGB.R + 30}, ${RGB.G + 30}, ${ RGB.B + 30}, 0)`,
-            backgroundColor_onHover: `rgba(${RGB.R - 32}, ${RGB.G - 32}, ${ RGB.B - 32}, 0.32)`,
-            backgroundColor_onActive: `rgba(${RGB.R - 32}, ${RGB.G - 32}, ${ RGB.B - 32}, 0.32)`,
+            border_onActive: `1px solid rgba(0, 0, 0, 0.32)`,
+            backgroundColor: `rgba(${RGB.R + 30}, ${RGB.G + 30}, ${
+              RGB.B + 30
+            }, 0)`,
+            backgroundColor_onHover: `rgba(${RGB.R}, ${RGB.G}, ${RGB.B}, 1)`,
+            backgroundColor_onActive: `rgba(${RGB.R}, ${RGB.G}, ${RGB.B}, 1)`,
           },
         };
       }
